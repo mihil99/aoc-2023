74da6578c63b2859250403111a438ac623f8f306
Michiel DW <michiel.dewilde@hotmail.com>
2024-01-31 20:26:23 +0100

    cleaning up

--- 1.c
+++ 1.c
@@ -8,8 +8,9 @@
 
 const char nums[9][6] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-int extract_num(char* c) {
-  if (isdigit(*c)) return *c - '0';
+int char_to_num(char* c, bool check_words) {
+  if ( isdigit(*c) ) return *c - '0';
+  if ( !check_words ) return -1;
   for (size_t i = 0; i < 9; i++)
   {
     if (strncmp(c, nums[i], strlen(nums[i])) == 0) return i+1;
@@ -17,20 +18,12 @@ int extract_num(char* c) {
   return -1;
 }
 
-int extract_number(char *line) {
+int extract_num_from_line(char *line, bool check_words) {
   char *first = line;
-  while( !isdigit(*first)) first++;
-  char *last = line + strlen(line);
-  while( !isdigit(*last)) last--;
-  return 10*(*first - '0') + (*last - '0');
-}
-
-int extract_number2(char *line) {
-  char *first = line;
-  while (extract_num(first) == -1) first++;
+  while (char_to_num(first, check_words) == -1) first++;
   char *last = line+strlen(line);
-  while (extract_num(last) == -1) last--;
-  return 10*(extract_num(first)) + extract_num(last);
+  while (char_to_num(last, check_words) == -1) last--;
+  return 10*(char_to_num(first, check_words)) + char_to_num(last, check_words);
 }
 
 int main()
@@ -38,29 +31,21 @@ int main()
   // PART 1
   const char *filename = "./1.input";
   FILE *fp = fopen(filename, "r");
-
   char line[LINE_LENGTH];
   int sum = 0;
-
   while( fgets(line, LINE_LENGTH, fp) != NULL) {
-    sum += extract_number(line);
+    sum += extract_num_from_line(line, false);
   }
-
   printf("Part 1: %d\n", sum);
   fclose(fp);
-
   // PART 2
   fp = fopen(filename, "r");
   sum = 0;
-
   while( fgets(line, LINE_LENGTH, fp) != NULL) {
-    int n = extract_number2(line);
+    int n = extract_num_from_line(line, true);
     sum += n;
-    // printf("%s => %d\n", line, n);
   }
-
   printf("Part 1: %d\n", sum);
   fclose(fp);
-
   return 0;
 }
--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -g
+NUMBER = 1
 
 all: my_program
 
-my_program: 1.c
-	$(CC) $(CFLAGS) -o my_program 1.c
+my_program: $(NUMBER).c
+	$(CC) $(CFLAGS) -o my_program $(NUMBER).c
 
 run: my_program
 	./my_program
 
 clean:
 	rm -f my_program
-
